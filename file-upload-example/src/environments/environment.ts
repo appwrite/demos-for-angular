37285c1c110050cf62aa3b2c7315177520af633f
benceHornyak <sir.bence15@gmail.com>
2020-10-03 21:47:54 +0200

    feat(env): intial dev env variables

--- file-upload-example/src/environments/environment.ts
+++ file-upload-example/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  endpoint: '', // replace with your own appwrite server ip
+  project: '' // replace with your own appwrite project id
 };
 
 /*
