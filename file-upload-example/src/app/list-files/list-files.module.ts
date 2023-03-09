8b2e3496acb788edbdd08537736c87e8b76d23eb
Bence Hornyák <sir.bence15@gmail.com>
2023-03-09 20:23:33 +0100

    chore: update material and ckd to v15

--- file-upload-example/src/app/list-files/list-files.module.ts
+++ file-upload-example/src/app/list-files/list-files.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ListFilesRoutingModule } from './list-files-routing.module';
 import { ListFilesComponent } from './list-files.component';
 import { ListFilesResolver } from './list-files.resolver';
-import { MatCardModule } from '@angular/material/card';
+import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 
 @NgModule({
   declarations: [ListFilesComponent],
